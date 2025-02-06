67aa5a99a0e6a4158ca0de36be337ecd14bde831
Code and Debug <codeanddebugedu@gmail.com>
2025-02-06 20:02:30 +0530

    space patterns solution

---
+++ 8. Patterns/pt5.py
@@ -0,0 +1,11 @@
+n = 13
+
+for i in range(1, n // 2 + 2):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+for i in range(n // 2, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
